71dc832b839839ddcd2a22ab8b2ec2e881af91cb
NAMBM <nambach2@Gmail.com>
2018-10-30 10:04:16 +0700

    Implement StringUtils for string comparing [NamBM]

--- src/main/java/repository/generic/constant/QueryConstant.java
+++
@@ -1,6 +0,0 @@
-package repository.generic.constant;
-
-public class QueryConstant {
-
-    public static final String TRUNCATE_TABLE = "truncate table ";
-}
--- src/main/java/repository/impl/RawBookRepositoryImpl.java
+++ src/main/java/repository/impl/RawBookRepositoryImpl.java
@@ -1,12 +1,40 @@
 package repository.impl;
 
 import model.RawBook;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import repository.RawBookRepository;
 import repository.generic.impl.GenericRepositoryImpl;
 
-public class RawBookRepositoryImpl extends GenericRepositoryImpl<RawBook> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RawBookRepositoryImpl extends GenericRepositoryImpl<RawBook> implements RawBookRepository {
 
     public RawBookRepositoryImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
+    }
+
+    @Override
+    public List<RawBook> searchByNameOrAuthor(String name) {
+        List<RawBook> list = new ArrayList<>();
+
+        if (name == null || name.trim().equals("")) {
+            return list;
+        }
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Query<RawBook> query = session.createQuery("FROM " + tableName + " b WHERE b.title LIKE :search_value OR b.authors LIKE :search_value", RawBook.class);
+
+            query.setParameter("search_value", "%" + name + "%");
+
+            list = query.list();
+
+            session.close();
+        } catch (Exception ignored) {
+        }
+        return list;
     }
 }
