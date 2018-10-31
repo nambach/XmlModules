27b97fed1aae52051584aaf4d3255cb3fd9ac441
NAMBM <nambach2@Gmail.com>
2018-10-31 18:19:55 +0700

    Improve performance of LCS algorithm by using StringUtils of Apache Common Lang [NamBM]

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -5,10 +5,12 @@ import model.LCSResult;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringUtils {
 
     private static final String DELIMITER_REGEX = "[\\s(),.;\\-]+";
+    private static final String DELIMITER_CHARS = " (),.;-|!?";
 
     private static final List<String> MISCELLANEOUS_WORDS = Arrays.asList(
             "tái bản", "bản đặc biệt", "bìa mềm", "tặng kèm", "số lượng có hạn", "2017", "2018");
@@ -18,8 +20,10 @@ public class StringUtils {
         sentence1 = "pre " + preProcessString(sentence1);
         sentence2 = "pre " + preProcessString(sentence2);
 
-        String[] arr1 = sentence1.split(DELIMITER_REGEX);
-        String[] arr2 = sentence2.split(DELIMITER_REGEX);
+        //String[] arr1 = sentence1.split(DELIMITER_REGEX);
+        //String[] arr2 = sentence2.split(DELIMITER_REGEX);
+        String[] arr1 = org.apache.commons.lang.StringUtils.split(sentence1, DELIMITER_CHARS);
+        String[] arr2 = org.apache.commons.lang.StringUtils.split(sentence2, DELIMITER_CHARS);
 
         int m[][] = new int[arr1.length][arr2.length];
 
@@ -43,8 +47,10 @@ public class StringUtils {
         sentence1 = "pre " + preProcessString(sentence1);
         sentence2 = "pre " + preProcessString(sentence2);
 
-        String[] S = sentence1.split(DELIMITER_REGEX);
-        String[] T = sentence2.split(DELIMITER_REGEX);
+        //String[] S = sentence1.split(DELIMITER_REGEX);
+        //String[] T = sentence2.split(DELIMITER_REGEX);
+        String[] S = org.apache.commons.lang.StringUtils.split(sentence1, DELIMITER_CHARS);
+        String[] T = org.apache.commons.lang.StringUtils.split(sentence2, DELIMITER_CHARS);
 
         int L[][] = new int[S.length][T.length];
         int z = 0;
@@ -73,24 +79,30 @@ public class StringUtils {
             }
         }
 
-//        System.out.println(ret.stream().collect(Collectors.joining(" ")));
+        //System.out.println(ret.stream().collect(Collectors.joining(" ")));
 
         return new LCSResult(S.length - 1, T.length - 1, z);
     }
 
+    // https://stackoverflow.com/questions/16228992/commons-lang-stringutils-replace-performance-vs-string-replace
     private static String preProcessString(String s) {
         s = s.trim().toLowerCase();
         for (String miscellaneousWord : MISCELLANEOUS_WORDS) {
-            s = s.replace(miscellaneousWord, "");
+            //s = s.replace(miscellaneousWord, "");
+            s = org.apache.commons.lang.StringUtils.replace(s, miscellaneousWord, "");
         }
         return s;
     }
 
-    public static void main(String[] args) {
-        String a = "Dep (so thang)";
-        String b = "Elle - Dep (so thang)";
+    private static void printArray(String[] arr) {
+        System.out.println(Arrays.stream(arr).collect(Collectors.joining(" ")));
+    }
 
-        double rs = calculateLCSubsequence(a, b).printOutInfo().calculateSimilarity();
+    public static void main(String[] args) {
+        String a = "Trưởng-Thành-Sau | Ngàn!? Lần Tranh Đấu";
+        String b = "Trưởng Thành Sau Ngàn Lần Tranh Đấu (Tái Bản 2018)";
+
+        double rs = calculateLCSubstring(a, b).printOutInfo().calculateIdentity();
         System.out.println(rs);
     }
 }
